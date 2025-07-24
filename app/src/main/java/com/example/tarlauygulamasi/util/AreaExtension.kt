290bd3115402d7c681a46684a268942b56903a41
Mert Alpay <mertalpay129@gmail.com>
2025-07-24 16:22:09 +0300

    fix

--- app/src/main/java/com/example/tarlauygulamasi/util/AreaExtension.kt
+++ app/src/main/java/com/example/tarlauygulamasi/util/AreaExtension.kt
@@ -1,7 +1,13 @@
 package com.example.tarlauygulamasi.util
 
-    fun Double.toFormattedArea():String {
-          return "%.2f dönüm".format(this/1000)
+import java.text.NumberFormat
+import java.util.Locale
+
+fun Double.toFormattedArea():String {
+
+        val formatter = NumberFormat.getNumberInstance(Locale.GERMANY)
+        formatter.maximumFractionDigits = 2
+        return "${formatter.format(this / 1000)} dönüm"
     }
 
 
